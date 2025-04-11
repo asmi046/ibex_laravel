9c0b7b92548d43215ee14e465c2f0cbd091974c0
asmi046 <asmi046@gmail.com>
2025-04-11 14:13:43 +0300

    +1

--- resources/views/components/product/main-card.blade.php
+++ resources/views/components/product/main-card.blade.php
@@ -1,5 +1,5 @@
 <div class="products-item">
-    <img src="{{ Storage::url('products/'.$item['img'])}}" alt="{{ $item['title'] }}" class="products-item__img">
+    <img src="{{ Storage::url($item['img'])}}" alt="{{ $item['title'] }}" class="products-item__img">
     <span class="products-item__text">
         <span class="products-item__caption">{{ $item['title'] }}</span>
         <span class="products-item__description">
